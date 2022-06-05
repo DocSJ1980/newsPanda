fd55513ae8704b9417d08c71fab70310f858da78
DocSJ1980 <docsaj@outlook.com>
2022-06-05 09:13:26 -0700

    NewsItem iteration created

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-  render() {
-    return (
-      <div>
-        This is news Item component.
+    
+  
+  
+    render() {
+    let {title, description, imageUrl, newsUrl} = this.props;
         
-      </div>
+        return (
+        <div className='my-3'>
+            <div className="card" style={{width: "18rem"}}>
+                <img src={imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}...</h5>
+                    <p className="card-text">{description}...</p>
+                    <a href={newsUrl} className="btn btn-primary btn-sm">Read More</a>
+                </div>
+            </div>
+        </div>
     )
   }
 }
