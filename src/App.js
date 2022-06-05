06a11511e025b67ce034fe7f744af269e6a99e54
DocSJ1980 <docsaj@outlook.com>
2022-06-05 02:23:57 -0700

    2nd Commit - news and newsitem components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar'
+import News from './components/News';
 
 export default class App extends Component {
-  c='john';
-  render() {
+ 
+    render() {
     return (
       <div>
-        Hello my first class based component {this.c}.
+          <NavBar />
+          <News />
       </div>
     )
   }
---
+++ src/components/NewsItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        This is news Item component.
+        
+      </div>
+    )
+  }
+}
+
+export default NewsItem
