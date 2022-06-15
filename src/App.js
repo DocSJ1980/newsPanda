c5185b26f15024dbe6c189ebd4ddb114debefdee
DocSJ1980 <docsaj@outlook.com>
2022-06-15 12:19:05 -0700

    7th Commit- Navigation bar categories added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import './App.css';
 
 import React, { Component } from 'react'
 import NavBar from './components/Navbar'
-import News from './components/News';
+import News from './components/News'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 export default class App extends Component {
  
     render() {
     return (
       <div>
+        <Router>
           <NavBar />
-          <News pageSize={9} country="us" category="sports"/>
+          <Routes>
+            <Route exact path="/" element={<News key='general' pageSize={9} country="us" category="general"/>}/>
+            <Route exact path="/business" element={<News key='business' pageSize={9} country="us" category="business"/>} />
+            <Route exact path="/entertainment" element={<News key='entertainment' pageSize={9} country="us" category="entertainment"/>}/>
+            <Route exact path="/health" element={<News key='health' pageSize={9} country="us" category="health"/>}/>
+            <Route exact path="/science" element={<News key='science' pageSize={9} country="us" category="science"/>}/>
+            <Route exact path="/sports" element={<News key='sports' pageSize={9} country="us" category="sports"/>}/>
+            <Route exact path="/technology" element={<News key='technology' pageSize={9} country="us" category="technology"/>}/>
+          </Routes>
+        </Router>
       </div>
     )
   }
